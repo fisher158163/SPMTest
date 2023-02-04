3945f1319052a9f5b126ecd66a0425baf31414b5
liyu158163 <18113334+liyu158163@users.noreply.github.com>
2023-02-04 12:21:18 +0800

    优化代码。
    优化代码。

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.4.3")
+        .package(url: "https://gitee.com/xhea/Alamofire", from: "5.4.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
