df947c5177fd86b6be7971a3e2c713bb20358ec1
Doraemon <132690170+MagicDoraemon@users.noreply.github.com>
2023-05-06 01:35:26 +0800

    新增Macro.swift (#1)
    新增Macro.swift。
    
    Co-authored-by: liyu158163 <18113334+liyu158163@users.noreply.github.com>

---
+++ Sources/SPMTest/Macro.swift
@@ -0,0 +1,17 @@
+//
+//  Macro.swift
+//  
+//
+//  Created by liyu on 2023/5/6.
+//
+
+import Foundation
+
+// 注意不同module访问需要将访问权限设为pubilc
+public struct Macro {
+    public var title = "Swift Package Module Test"
+    
+    public init() {
+        
+    }
+}
